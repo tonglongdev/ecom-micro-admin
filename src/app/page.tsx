e20c07913428962ccdcbefaeb26f82da4b6cc82e
Tong Long Dev <tonglong.dev@gmail.com>
2025-12-17 13:49:10 +0700

    CardList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 const Homepage = () => {
   return (
@@ -9,10 +10,16 @@ const Homepage = () => {
         <AppBarChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
         <AppPieChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
       </div>
     </div>
   );
