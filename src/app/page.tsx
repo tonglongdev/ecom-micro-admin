d8e9a1705d83addbaf350de5736b775fcbeb9c71
Tong Long Dev <tonglong.dev@gmail.com>
2025-12-17 12:57:38 +0700

    AppAreaChart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 
 const Homepage = () => {
@@ -5,6 +6,9 @@ const Homepage = () => {
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
       </div>
     </div>
   );
