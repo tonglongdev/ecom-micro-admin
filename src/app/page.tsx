0b400644ea001ca7c7cd2366953b9c5990955e85
Tong Long Dev <tonglong.dev@gmail.com>
2025-12-17 12:46:29 +0700

    AppBarChart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import AppBarChart from "@/components/AppBarChart";
+
 const Homepage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <h1>homepage</h1>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
     </div>
   );
 };
