187cf904e5ada371ecfdf3f0d966e4e41b15d60c
Tong Long Dev <tonglong.dev@gmail.com>
2025-12-17 14:11:19 +0700

    todolist + popover calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
 const Homepage = () => {
   return (
@@ -14,6 +15,9 @@ const Homepage = () => {
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
         <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <TodoList />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
